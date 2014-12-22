112e3dcedba813ba0adb60ab45ca1f2aba822778
Erin Kim <erin.e.kim@gmail.com>
2014-12-22 05:11:11 +0000

    Added Video Uploading feature on Lessons page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141221193549) do
+ActiveRecord::Schema.define(version: 20141222050522) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20141221193549) do
     t.integer  "section_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "video"
   end
 
   add_index "lessons", ["section_id"], name: "index_lessons_on_section_id", using: :btree
